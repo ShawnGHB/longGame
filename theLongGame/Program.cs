1181cbcaa3b7783c237919458fa0d8fe59a38598
ShawnGHB <shawnshawn4116@gmail.com>
2024-11-11 00:08:44 -0500

    Created theLongGame Solution
    Added Visual Studio File called "theLongGame"

---
+++ theLongGame/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace theLongGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            string userInp = "";
+
+        }
+    }
+}
