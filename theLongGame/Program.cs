23192d601e7b091f16d40421a5dc6c266806f969
ShawnGHB <shawnshawn4116@gmail.com>
2024-11-11 02:40:49 -0500

    Added in KeyPress Game
    Made it so theLongGame has a keypress game, will be followed by file saving capabilities

--- theLongGame/Program.cs
+++ theLongGame/Program.cs
@@ -7,6 +7,29 @@
             int score = 0;
             string userInp = "";
 
+            //Ask User for Input
+            Console.WriteLine("Welcome user! What's Your name?");
+            userInp = Console.ReadLine();
+            Console.WriteLine("Well, " + userInp + " Press every key EXCEPT enter");
+
+            do
+            {
+                //will start the adding process only if score != 0
+                if (score != 0)
+                {
+                    Console.WriteLine("\nGreat! " + userInp + " has earned " + score + " points!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Well go on! Start pressing!");
+                    score++;
+                }
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Enter && !Console.KeyAvailable);
+
+            Console.WriteLine("Save Codee");
+
         }
     }
 }
